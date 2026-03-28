65d2fc1ed6fc26fd2de63dc2b638d91814e53ad2
IAmOJohnson <Obafemiakinpelu@gmail.com>
2026-03-28 04:22:27 +0100

    Final PWA and Icon fix

--- marketplace/templates/marketplace/sw.js
+++ marketplace/templates/marketplace/sw.js
@@ -9,7 +9,7 @@ const STATIC_ASSETS = [
   '/',
   '/static/css/oja.css',
   '/static/img/oja_logo.png',
-  '/static/img/icons/icon-192.svg',
+  '/static/img/oja_logo.png',
   '/static/manifest.json',
   '/offline/',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap',
--- ojac/urls.py
+++ ojac/urls.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('marketplace.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 # Serve PWA files at root level
 from django.views.generic import TemplateView
 from django.contrib.staticfiles.storage import staticfiles_storage
@@ -16,6 +10,24 @@ import os
 from django.conf import settings as _settings
 
 from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('marketplace.urls')),
+# Added name='manifest' and name='sw' so the {% url %} tags work
+    path('manifest.json', TemplateView.as_view(
+        template_name='marketplace/manifest.json', 
+        content_type='application/json'
+    ), name='manifest'),
+
+    path('sw.js', TemplateView.as_view(
+        template_name='marketplace/sw.js', 
+        content_type='application/javascript'
+    ), name='sw'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
 urlpatterns += [
     # We use TemplateView to serve these as if they are at the root
